5ef098ff74b93209c91378812075b78f07ce8e2d
Oleksiy Nosov <oleksiynosov@gmail.com>
2017-07-11 20:10:21 +0300

    Removed all tabs

--- fact.rb
+++ fact.rb
@@ -1,20 +1,20 @@
 def fact numb = 0
-	numb <= 1 ?	1 : numb * fact(numb - 1)
+  numb <= 1 ? 1 : numb * fact(numb - 1)
 end
 
 def fact_normal numb = 0
-	i = 0
-	mlt = 1
-	while numb > 1
-		mlt *= numb
-		numb -= 1
-	end
-	mlt
+  i = 0
+  mlt = 1
+  while numb > 1
+    mlt *= numb
+    numb -= 1
+    end
+  mlt
 end
 
 for i in 0..10 do
-	puts fact(i)
-	puts fact_normal(i)
+  puts fact(i)
+  puts fact_normal(i)
 end
 
 
--- sort.rb
+++ sort.rb
@@ -1,12 +1,12 @@
 def buble_sort array = []
-	for i in 0...array.length
-		for j in 0...(array.length - i - 1)
-			if array[j] > array[j + 1]
-				array[j], array[j + 1] = array[j + 1], array[j]
-			end
-		end
-	end
-	array
+  for i in 0...array.length
+    for j in 0...(array.length - i - 1)
+      if array[j] > array[j + 1]
+      array[j], array[j + 1] = array[j + 1], array[j]
+      end
+    end
+  end
+  array
 end
 
 array = (-10..10).to_a.shuffle
